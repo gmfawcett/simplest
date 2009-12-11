9441a8613dd585c7fa120f844c3ad00641c215fd
Graham Fawcett <fawcett@uwindsor.ca>
2009-12-11 14:18:30 -0500

    added simplest/servlet.clj; added deps and compile path to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject simplest "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+		 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+		 [javax.servlet/servlet-api "2.5"]]
+  :compile-path "target/classes")
+
+
---
+++ src/simplest/servlet.clj
@@ -0,0 +1,9 @@
+(ns simplest.servlet           
+  (:import (java.util Date))
+  (:gen-class :extends javax.servlet.http.HttpServlet))
+
+(defn -doGet [req resp]
+  (let [title "Simple, but it works!"]
+    (doto (.getWriter resp)
+      (.println (format "<html><head><title>%s</title></head>" title))
+      (.println (format "<body><h1>%s</h1><p>%s</p></body></html>" title (Date.))))))
