dea4db3bbf3d7a26d0dc85ecace46c41cacd11bd
Graham Fawcett <fawcett@uwindsor.ca>
2009-12-11 14:25:59 -0500

    * src/simplest/servlet.clj: oops, wrong number of args, forgot the 'this'

--- src/simplest/servlet.clj
+++ src/simplest/servlet.clj
@@ -2,7 +2,7 @@
   (:import (java.util Date))
   (:gen-class :extends javax.servlet.http.HttpServlet))
 
-(defn -doGet [req resp]
+(defn -doGet [this req resp]
   (let [title "Simple, but it works!"]
     (doto (.getWriter resp)
       (.println (format "<html><head><title>%s</title></head>" title))
